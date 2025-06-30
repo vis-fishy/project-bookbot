78c665c40c2dbdf284a51d00af5f2716b1f88bb6
Elvis Sestomi <sestomielvis@gmail.com>
2025-06-30 12:36:59 +0700

    added try and except method to main.py

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
+#!C:\Users\user\AppData\Local\Programs\Python\Launcher\py.exe
 from stats import *
+import sys
 
-def get_book_text(file_path=None):
-    if (file_path == None):
-        print("Error: filepath not defined")
-        return None
+def get_book_text(file_path):
     with open(file_path, "r", encoding="utf-8") as f:
         return f.read()
 
 def main():
-    filepath = "books/frankenstein.txt"
-    text_result = get_book_text(filepath)
-
-    if (filepath != None):
+    try:
         #text file name section
+        filepath = sys.argv[1]
+        text_result = get_book_text(filepath)
         string_filepath = filepath.split("/")
         name = ""
         for string in string_filepath:
@@ -27,5 +25,15 @@ def main():
         print(f"There are {count_words} words in this {name} text file")
         for item in sorted_list_dict:
             print(f"{item["name"]} : {item["value"]}")
-
+        sys.exit(0)
+    
+    except IndexError:
+        print("IndexError: filepath not defined")
+        print("Usage : py main.py <path_to_file>")
+        sys.exit(1)
+    
+    except Exception as gen:
+        print(gen)
+        print("Usage : py main.py <path_to_file>")
+        sys.exit(1)
 main()
